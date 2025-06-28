adf785ad97e413f94f3609f442648fcccb7cebe3
lmc nova <164868902+lmcnova@users.noreply.github.com>
2025-06-28 11:37:42 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,11 @@
 
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(
+        'mongodb+srv://vrvimalraj04:s1WarkceHiLQ47jh@cluster0.edetq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+        tls=True,
+        tlsAllowInvalidCertificates=True
+    )
 db = client["chatdb"]
 users_collection = db["users"]
 messages_collection = db["messages"]
