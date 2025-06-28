80a20925e2c44cdaf015ee9e2921314be2c9dcb6
lmc nova <164868902+lmcnova@users.noreply.github.com>
2025-06-28 11:59:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,4 @@ if __name__ == "__main__":
     # Initialize SocketIO with the Flask app
     socketio.init_app(app, cors_allowed_origins="*")
     # Run the app with SocketIO support
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=True, allow_unsafe_werkzeug=True, host='0.0.0.0')
